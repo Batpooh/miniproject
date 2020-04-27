75af3ac3eb658facf5c9976ceeccf17924165e1d
Batpooh <lionpoohza@gmail.com>
2020-04-27 14:41:46 +0700

    First

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const Config = {
+    apiKey: "AIzaSyBxcx2PAvo135jiit2FkHKk5_rlnz1o5mw",
+    authDomain: "miniproject-7181c.firebaseapp.com",
+    databaseURL: "https://miniproject-7181c.firebaseio.com",
+    projectId: "miniproject-7181c",
+    storageBucket: "miniproject-7181c.appspot.com",
+    messagingSenderId: "588882895200",
+    appId: "1:588882895200:web:cabe1fc6beb5acb05dbe05",
+    measurementId: "G-9D4K4CFD6K"
+  };
+
+  export default Config
